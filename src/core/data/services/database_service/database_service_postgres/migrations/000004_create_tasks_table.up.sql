d41e1aaa1bf0c2404c3f2f5f3f82b6a31a3abbf4
Javier Zavala <javierzavalaponce@gmail.com>
2026-01-14 10:07:32 -0600

    add migrations new table: task: taskss

---
+++ src/core/data/services/database_service/database_service_postgres/migrations/000004_create_tasks_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS test_backend.tasks (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    completed BOOLEAN DEFAULT false,
+    usuario_id UUID NOT NULL REFERENCES test_backend.users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
