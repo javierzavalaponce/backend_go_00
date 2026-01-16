af8bb241edf39823527614643a61fb1142ea828f
Javier Zavala <javierzavalaponce@gmail.com>
2026-01-15 23:23:30 -0600

    add fix

--- src/features/tasks/data/repositories/tasks_repository_postgres/tasks_repository_postgres.go
+++ src/features/tasks/data/repositories/tasks_repository_postgres/tasks_repository_postgres.go
@@ -2,7 +2,6 @@ package tasks_repository_postgres
 
 import (
 	"database/sql"
-	"fmt"
 
 	tasks_models "github.com/DEINSI-DEVELOP/test_backend_go.git/src/features/tasks/domain/models"
 	task_repository "github.com/DEINSI-DEVELOP/test_backend_go.git/src/features/tasks/domain/repositories"
@@ -21,17 +20,11 @@ func NewTasksRepositoryPostgres(db *sql.DB) task_repository.TaskRepository {
 
 func (u *TaskRepositoryPostgres) Create(task *tasks_models.Task, userUUID uuid.UUID) (uuid.UUID, error) {
 	query := `INSERT INTO test_backend.tasks (name, description, user_id) VALUES ($1, $2, $3) RETURNING id`
-
-	userUUID, e := uuid.Parse(task.User_ID)
-	if e != nil {
-		return fmt.Errorf("user_id inválido: %w", e)
-	}
-
-	var id uuid.UUID
-	err := u.db.QueryRow(query, task.Title, task.Description, userUUID).Scan(&id)
+	var taskId uuid.UUID
+	err := u.db.QueryRow(query, task.Title, task.Description, userUUID).Scan(&taskId)
 	if err != nil {
-		return id, err
+		return taskId, err
 	}
 
-	return id, nil
+	return taskId, nil
 }
